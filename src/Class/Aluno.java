a39144d15127a74364d1569fe26af70ccc37f5e9
Oto Frias <otogf@hotmail.com>
2026-05-16 10:08:30 -0300

    Classe pessoa

---
+++ src/Class/Aluno.java
@@ -0,0 +1,19 @@
+public class Aluno extends Pessoa {
+    private String matricula;
+
+    public Aluno(String matricula) {    
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
+    public void ExibirDetalhes(){
+        
+    }
+}
---
+++ src/Class/Pessoa.java
@@ -0,0 +1,43 @@
+public abstract class Pessoa {
+    private String nome;
+    private String email;
+    Endereco Endereco;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String email, Endereco endereco) {
+        this.nome = nome;
+        this.email = email;
+        Endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        Endereco = endereco;
+    }
+
+    public void ExibirDetalhes() {
+
+    }
+
+}
