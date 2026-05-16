55f802e2bd7a3c2346914d9d784cc0cf59ca0467
Oto Frias <otogf@hotmail.com>
2026-05-16 10:18:20 -0300

    Class Endereço

---
+++ src/Class/Endereco.java
@@ -0,0 +1,32 @@
+package Class;
+
+public class Endereco {
+    private String rua;
+    private String cidade;
+    private String cep;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+}
